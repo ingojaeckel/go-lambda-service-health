b45c07d2fd9f97302d89195816c3c72cb498ee92
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2018-01-18 06:57:01 -0800

    added outline for adding new measurements to existing data

---
+++ merge/merge.go
@@ -0,0 +1,13 @@
+package merge
+
+func GetExistingData() {
+	// (1) get from s3
+	// (2) uncompress
+	// (3) parse
+}
+
+func UpdateMeasurements() {
+	// (1) serialize new measurements, append to existing data
+	// (2) compress
+	// (3) upload
+}
