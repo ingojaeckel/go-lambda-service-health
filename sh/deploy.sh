771ee0be007f0b7964c094c01188afbce80c0d00
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2018-01-28 21:39:49 -0800

    update function if it already exists

--- sh/deploy.sh
+++ sh/deploy.sh
@@ -6,10 +6,27 @@ AWS_ACCOUNT_ID=$3
 ROLE=$4
 DEPLOYMENT_ARTIFACT=$5
 
-aws lambda create-function					\
-	--region ${REGION}					\
-	--function-name ${FN_NAME}				\
-	--zip-file fileb://./${DEPLOYMENT_ARTIFACT}		\
-	--runtime go1.x						\
-	--role arn:aws:iam::${AWS_ACCOUNT_ID}:role/${ROLE} 	\
-	--handler main
+echo "Checking if function exists..."
+aws lambda get-function --region ${REGION} --function-name $FN_NAME
+
+if [[ $? -eq "0" ]]; then
+    echo "Updating function ${FN_NAME} .."
+
+    aws lambda update-function-code					\
+        --region ${REGION}					\
+        --function-name ${FN_NAME}				\
+        --zip-file fileb://./${DEPLOYMENT_ARTIFACT}		\
+        --runtime go1.x						\
+        --role arn:aws:iam::${AWS_ACCOUNT_ID}:role/${ROLE} 	\
+        --handler main
+else
+    echo "Creating function ${FN_NAME}.."
+
+    aws lambda create-function				                \
+    	--region ${REGION}					                \
+    	--function-name ${FN_NAME}				            \
+    	--zip-file fileb://./${DEPLOYMENT_ARTIFACT}		    \
+    	--runtime go1.x						                \
+    	--role arn:aws:iam::${AWS_ACCOUNT_ID}:role/${ROLE} 	\
+    	--handler main
+fi
